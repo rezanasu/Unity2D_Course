b3d968c2aa44f24d3ea0700dff2f37bf2140f727
Reza Nasution <rezanasution82@gmail.com>
2018-11-15 03:49:47 +0100

    Udemy2D_Course

--- Number Wizard [Console]/Assets/Scripts/NumberWizard.cs
+++ Number Wizard [Console]/Assets/Scripts/NumberWizard.cs
@@ -4,46 +4,63 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
 
-    private int max = 1000;
-    private int min = 1;
-    private int guess = 500;
+    private int max;
+    private int min;
+    private int guess;
 
 
     // Use this for initialization
     void Start () {
 
-        
+        StartGame();
+       
+	}
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
         Debug.Log("Welcome to Number Wizard...");
         Debug.Log("Pick an number, don't tell me what it is...");
         Debug.Log("The highest number you can pick is " + max);
         Debug.Log("The lowest number you can pick is " + min);
         Debug.Log("Tell me if your number is higher or lower than " + guess);
         Debug.Log("Push Up =  Higher, Push Down = Lower, Push Enter = Correct");
-	}
+        max = max + 1;
+        min = min - 1;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("Up Arrow key was pressed.");
+           
             min = guess;
-            Debug.Log(guess);
+            NextGuess();
         }
 
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Down Arrow key was pressed.");
+            
             max = guess;
-            Debug.Log(guess);
+            NextGuess();
+
         }
 
         else if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("Correct!");
+            StartGame();
         }
 
+    }
 
-
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is it higher or lower than... " + guess);
     }
 }
